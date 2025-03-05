f1447106a5e8c85cacc9039e3082278fce081dd3
yashvaghasiya119 <yashvaghasiya360@gmail.com>
2025-03-05 18:34:14 +0530

    add slash path

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,9 @@ app.use(cors({
 app.use(cookieParser()  )
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use("/" , (req,res)=>{
+    return res.send("hello")
+})
 app.use("/user" , userroute )
 app.use("/todo" , checklogin,todoroute )
 
