0a3247453e482a99750b737f8b2833222d644777
Bhavnish Patel <patelbhavnish92@gmail.com>
2025-03-06 11:22:19 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,18 @@ connect().then(()=>{
 })
 // app.use(cors())
 
-app.use(cors());
+app.use(cors({
+  origin: "https://todoappfr.vercel.app", // Allow only your frontend
+  credentials: true // Allow cookies and authentication headers
+}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://todoappfr.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(cookieParser()  )
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
